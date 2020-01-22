269de9585402ad4ae27b33c6844938884fbc0ac9
Wolf-IV <zacharie.sulmont@gmail.com>
2020-01-22 16:47:05 +0100

    donjon mise a jour

--- src/main/java/Wolf_IV/magmabeo/structures/Dungeon.java
+++ src/main/java/Wolf_IV/magmabeo/structures/Dungeon.java
@@ -11,7 +11,7 @@ public class Dungeon {
 	public static void init(World world, int x, int y, int z) {
 int i;
 		Random rand = new Random();
-		if (world.getBlock(x, y, z)== Blocks.nether_brick && world.getBlock(x, y+1 ,z)== Blocks.air && Math.random()<0.25) {
+		if (world.getBlock(x, y, z)== Blocks.nether_brick && world.getBlock(x, y+1 ,z)== Blocks.air) {
 			for(i=0;i<5;i++) {
 			world.setBlock(x+2, y+i, z, Blocks.cobblestone_wall);
 			world.setBlock(x-2, y+i, z, Blocks.cobblestone_wall);
@@ -38,16 +38,17 @@ int i;
 			//out 1
 			world.setBlock(x+2, y, z+2, Blocks.cobblestone);
 			world.setBlock(x-2, y, z+2, Blocks.cobblestone);
-			world.setBlock(x, y+3, z+2, magmabeo.Titan_Ore);
+			world.setBlock(x, y+5, z+2, Blocks.cobblestone);
+			world.setBlock(x, y+1, z+2, magmabeo.Titan_Block);
 			//out 1
 			//down
-			for(int ii=-1;ii<60;i++) {
+			for(int ii=0;ii<y-16;ii++) {
 				for(i=-1;i<2;i++) {
-					world.setBlock(x+i, y-ii, z+ii+4, Blocks.cobblestone);
-					world.setBlock(x+i, y-ii-1, z+ii+4, Blocks.air);//c-a-a-a-c le chemin
-					world.setBlock(x+i, y-ii-2, z+ii+4, Blocks.air);
-					world.setBlock(x+i, y-ii-3, z+ii+4, Blocks.air);
-					world.setBlock(x+i, y-ii-4, z+ii+4, Blocks.cobblestone);
+					world.setBlock(x+i, y-ii, z+ii+2, Blocks.cobblestone);
+					world.setBlock(x+i, y-ii-1, z+ii+2, Blocks.air);//c-a-a-a-c le chemin
+					world.setBlock(x+i, y-ii-2, z+ii+2, Blocks.air);
+					world.setBlock(x+i, y-ii-3, z+ii+2, Blocks.air);
+					world.setBlock(x+i, y-ii-4, z+ii+2, Blocks.cobblestone);
 				}
 			}
 			
